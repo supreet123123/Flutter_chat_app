611229b4506153d67b31da9482e6d61df789519c
Supreet Ganguly <96295642+supreet123123@users.noreply.github.com>
2021-12-29 12:19:18 +0530

    Add files via upload

--- lib/test/unit_test.dart
+++ lib/test/unit_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:firebase_chat_example/main.dart';
 
 void main(){
-  test('test sum', () {
+  test('test calc', () {
     expect(calc(2),4);
 
   });
